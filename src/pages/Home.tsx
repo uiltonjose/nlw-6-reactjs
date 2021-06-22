7276964964f463d96df34bc0c3d2c86e1d37371f
Uilton Santos <uilton.jose@gmail.com>
2021-06-22 17:33:23 +0100

    Authentication with firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { firebase, auth } from '../services/firebase';
 
 import iLustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -11,8 +12,16 @@ import '../styles/auth.scss';
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push('/rooms/new');
+    }).catch(error => {
+      console.error(error)
+    })
+
   }
 
   return (
@@ -26,7 +35,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Login with Google" />
             Crie sua sala com a Google
           </button>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,17 +3,29 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAZlcqYWH9BG9m-I35t8iFGtBAVKEC4Mi0",
+  authDomain: "nlw6--letmeask.firebaseapp.com",
+  databaseURL: "https://nlw6--letmeask-default-rtdb.firebaseio.com",
+  projectId: "nlw6--letmeask",
+  storageBucket: "nlw6--letmeask.appspot.com",
+  messagingSenderId: "973248988209",
+  appId: "1:973248988209:web:c750e5fc77461d554ec110"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
