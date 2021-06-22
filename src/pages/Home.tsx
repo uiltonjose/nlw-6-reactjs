71a56fd1c88e3ac30ecf00527b4a350e47ce1dc4
Uilton Santos <uilton.jose@gmail.com>
2021-06-22 18:09:47 +0100

    Implementation of: - Context Authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { firebase, auth } from '../services/firebase';
+
+import { AuthContext } from '../App';
 
 import iLustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -11,17 +13,14 @@ import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
 
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result);
-      history.push('/rooms/new');
-    }).catch(error => {
-      console.error(error)
-    })
-
+    history.push('/rooms/new');
   }
 
   return (
